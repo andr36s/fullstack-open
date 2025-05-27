58bed5ed04d94cfffc3c403e0f344bfc3e489541
andr36s <andres40106@gmail.com>
2025-05-27 15:13:57 -0500

    Se realiza el proceso 1.2: Información del Curso, paso 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,27 +1,44 @@
-const Hello = (props) => {
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
 
-  console.log(props)
+const Part = ({ name, exercises }) => {
+  return (
+    <p>{name} {exercises}</p>
+  )
+}
+
+const Content = ({parts}) => {
   return (
     <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <Part name={parts[0].part1} exercises={parts[0].exercises1} />
+      <Part name={parts[1].part2} exercises={parts[1].exercises2} />
+      <Part name={parts[2].part3} exercises={parts[2].exercises3} />
     </div>
   )
 }
 
-const App = () => {
+const Total = ({parts}) => {
+  return (
+    <p>Number of exercises {parts[0].exercises1 + parts[1].exercises2 + parts[2].exercises3}</p>
+  )
+}
 
-  const name = 'Peter'
-  const age = 10
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { part1: 'Fundamentals of React', exercises1: 10},
+    { part2: 'Using props to pass data', exercises2: 7},
+    { part3: 'State of a component', exercises3: 14 }
+  ]
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
