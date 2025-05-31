7562bc956764e5f0cc77c2633cb01856b7c53f14
andr36s <andres40106@gmail.com>
2025-05-31 14:59:57 -0500

    Realizo la actividad 1.13*: anecdotes, paso 2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -115,18 +115,38 @@ const App = () => {
     }
   ] 
   }
-
   const [counter, setCounter] = useState({
     good: 0,
     neutral: 0,
     bad: 0
   })
-
   const stadistics = calculateStadistics(counter)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
   const handleGoodClick = () => setCounter({ ...counter, good: counter.good + 1 })
   const handleNeutralClick = () => setCounter({ ...counter, neutral: counter.neutral + 1 })
   const handleBadClick = () => setCounter({...counter, bad: counter.bad + 1})
+  const handleNextAnecdoteClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  }
+  const handleVoteClick = () => {
+    const newVote = [...votes]
+    newVote[selected] += 1;
+    setVotes(newVote);
+  }
 
   return (
     <div>
@@ -135,12 +155,18 @@ const App = () => {
       <Content course={course} />
       <Total course={course} />
 
-      <h1>De la tarea 1.6 a 1.14</h1>
+      <h1>De la tarea 1.6 a 1.11</h1>
       <h2>Danos un comentario</h2>
       <Button onClick={handleGoodClick} text='Good' />
       <Button onClick={handleNeutralClick} text='Neutral' />
       <Button onClick={handleBadClick} text='Bad' />
       <History stadistics={stadistics} counter={counter} />
+
+      <h1>De la tarea 1.12 a 1.14</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>Has votado {votes[selected]} veces</p>
+      <button onClick={handleVoteClick}>Votes</button>
+      <button onClick={handleNextAnecdoteClick}>Siguiente anecdota</button>
     </div>
   )
 }
